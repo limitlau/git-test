7cf970e2960320562fa7ae7962252fa89ffdd3d5
Lau <limitlau@qq.com>
2022-08-17 19:44:55 +0800

    second commit

--- src/main/java/com/lau/git/GitTest.java
+++ src/main/java/com/lau/git/GitTest.java
@@ -3,5 +3,6 @@ package com.lau.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git!!");
     }
 }
